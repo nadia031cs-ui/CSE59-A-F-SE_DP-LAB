8af9ba55e9a7db2e8305d32f6cbb9fa9ffe53709
Nadia Muntaha <nadia031cs@gmail.com>
2026-06-09 11:24:30 +0600

    Update YouDo8.java

--- Task 2/YouDo8.java
+++ Task 2/YouDo8.java
@@ -1,1 +1,33 @@
-
+class Item{
+    String name;
+    double price;
+    
+    Item(String name,double price){
+        this.name=name;
+        this.price=price;
+    }
+}
+class order{
+    Item item1;
+    Item item2;
+    
+order(Item item1,Item item2){
+        this.item1=item1;
+        this.item2=item2;
+    }
+    
+    void printBill(){
+        double totalCost = item1.price + item2.price;
+        System.out.println("Total Cost: $"+ totalCost);
+    
+    }
+}
+public class Main {
+    public static void main(String[] args){
+        Item mouse = new Item("Mouse",20.0);
+        Item keyboard= new Item("Keyboard",45.0);
+        order order = new order(mouse, keyboard);
+        order.printBill();
+    
+}
+}
