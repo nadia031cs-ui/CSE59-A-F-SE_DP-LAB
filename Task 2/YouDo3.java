70dcd4e605ab3c9bd6ff1cbf80f5bf5606af72b8
Nadia Muntaha <nadia031cs@gmail.com>
2026-06-09 10:37:48 +0600

    Update YouDo3.java

--- Task 2/YouDo3.java
+++ Task 2/YouDo3.java
@@ -1,1 +1,14 @@
-
+class Countdown{
+    void start(){
+        for(int i=5;i>=1;i--){
+            System.out.println(i);
+        }
+        System.out.println("Go!");
+    }
+}
+public class Main1{
+    public static void main(String[] args){
+        Countdown c=new Countdown();
+        c.start();
+    }
+}
