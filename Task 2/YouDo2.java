e1da02843f395a01b12e654690d6aca51e8a02db
Nadia Muntaha <nadia031cs@gmail.com>
2026-06-09 10:31:41 +0600

    Update YouDo2.java

--- Task 2/YouDo2.java
+++ Task 2/YouDo2.java
@@ -1,1 +1,12 @@
-
+class NumberChecker{
+    void check(int num){
+        boolean result = (num%2==0) && (num>10);
+        System.out.println(result);
+    }
+}
+public class Main1{
+    public static void main(String[] args){
+        NumberChecker c=new NumberChecker();
+        c.check(14);
+    }
+}
