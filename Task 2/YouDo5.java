6cbb74a56d95fd11da65e16b193f725e085a0b21
Nadia Muntaha <nadia031cs@gmail.com>
2026-06-09 10:52:54 +0600

    Update YouDo5.java

--- Task 2/YouDo5.java
+++ Task 2/YouDo5.java
@@ -1,1 +1,15 @@
-
+class Printer{
+    void printData(String text){
+        System.out.println("Text: "+text);
+    }
+    void printData(int number){
+        System.out.println("Number: "+number);
+    }
+}
+public class Main1{
+    public static void main(String[] args) {
+        Printer p = new Printer();
+        p.printData("Hello, World!");
+        p.printData(100);
+    }
+}
