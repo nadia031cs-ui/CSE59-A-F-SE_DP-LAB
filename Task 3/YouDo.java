4de52ddec2e01f59291ca0570fd4dca6deed76c0
Nadia Muntaha <nadia031cs@gmail.com>
2026-06-16 10:13:15 +0600

    Add User, EmailValidator, and UserRepository classes

--- Task 3/YouDo.java
+++ Task 3/YouDo.java
@@ -1,1 +1,48 @@
-
+class User{
+    private String username;
+    private String email;
+    
+    public User (String username,String email){
+        this.username=username;
+        this.email=email;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getEmail(){
+        return email;
+    }
+}
+ 
+class EmailValidator{
+     
+     public boolean isValid(String email){
+          if(email != null && email.contains("@")){
+              return true;
+          }
+          
+          System.out.println("Invalid email format.");
+          return false;
+     }
+}
+class UserRepository{
+     public void save(User user){
+           System.out.println("Connecting to database...");
+             System.out.println("Saving user" + user.getUsername()+"to the users table.");
+             
+     }
+}
+public class Main{
+    
+    
+    public static void main(String[] args){
+        User user = new User ("nadia" , "nadia@example.com");
+        
+        EmailValidator validator = new  EmailValidator();
+        UserRepository repository = new    UserRepository();
+        
+        if(validator.isValid(user.getEmail())){
+            repository.save(user);
+        }
+    }
+}
