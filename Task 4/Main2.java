03ed856389cf1b9f3437511c652979359b859eb6
Nadia Muntaha <nadia031cs@gmail.com>
2026-06-23 10:29:07 +0600

    Update Main2.java

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,33 @@
+class TeamMember{
+    protected String name;
+    
+    public TeamMember(String name){
+        this.name=name;
+    }
+}
 
+interface BonusEligible{
+    void calculateBonus();
+}
+class Employee extends TeamMember implements BonusEligible{
+    public Employee(String name){
+        super(name);
+    }
+    @Override
+    public void calculateBonus(){
+        System.out.println("Calculating standard employee bonus....");
+    }
+}
+class Contractor extends TeamMember{
+    public Contractor(String name){
+        super(name);
+    }
+}
+public class Main{
+    public static void main(String[] args){
+        Employee emp = new Employee("Alice");
+        Contractor con= new Contractor("Bob");
+        emp.calculateBonus();
+        System.out.println(con.name+"is a contrcator and doesnot receive bonus. ");
+    }
+}
