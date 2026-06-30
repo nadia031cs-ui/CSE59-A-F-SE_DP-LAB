a2e32c83c200645ab4c4f302f813937ff59ae334
Nadia Muntaha <nadia031cs@gmail.com>
2026-06-30 11:07:22 +0600

    Update Main3.java

--- Task 5/Main3.java
+++ Task 5/Main3.java
@@ -1,1 +1,45 @@
 
+class Patient{
+    private String patientName;
+    private String nationalId;
+
+    public Patient(String patientName, String nationalId){
+        this.patientName=patientName;
+        this.nationalId=nationalId;
+    }
+    public String getPatientName(){
+        return patientName;
+    }
+    public String getNationalId(){
+        return nationalId;
+    }
+}
+class IdValidator{
+    public boolean validateId(String nationalId){
+        return nationalId.length()==10 || nationalId.length()==17;
+    }
+}
+class SmsService{
+    public void sendSmsConfirmation(Patient patient, IdValidator validator){
+
+        if(validator.validateId(patient.getNationalId())){
+            System.out.println("Sendig SMS to "+ patient.getPatientName()+ ":Registration Successful");
+
+        }
+        else
+        {
+            System.out.println("Invalid National ID. Registration Failed.");
+        }
+    }
+}
+
+public class Main4 {
+    public static void main(String[] args) {
+        Patient patient= new Patient("Nadia","1234567890");
+        IdValidator validator= new IdValidator();
+        SmsService smsService= new SmsService();
+
+        smsService.sendSmsConfirmation(patient, validator);
+    }
+    
+}
