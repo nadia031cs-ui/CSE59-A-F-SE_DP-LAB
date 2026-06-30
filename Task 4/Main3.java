5ad1f9594a4dde7216a54ac0d2a55d983e1aaa79
Nadia Muntaha <nadia031cs@gmail.com>
2026-06-30 09:06:56 +0600

    Implement printer, scanner, and fax interfaces

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,46 @@
+public interface Printer {
 
+    void printDocument();
+
+}
+public interface Scanner {
+
+    void scanDocument();
+}
+public interface FaxMachine {
+
+    void faxDocument();
+
+}
+public class MultiFunctionCopier implements Printer, Scanner, FaxMachine {
+@Override
+ public void printDocument() {
+
+        System.out.println("Printing...");
+
+    }
+@Override
+public void scanDocument() {
+
+        System.out.println("Scanning...");
+
+    }
+@Override
+
+    public void faxDocument() {
+
+        System.out.println("Faxing...");
+
+    }
+
+}
+public class BasicPrinter implements Printer {
+ @Override
+
+    public void printDocument() {
+
+        System.out.println("Printing...");
+
+    }
+
+}
