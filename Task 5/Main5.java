31f8c3513d66b047fe66897bc0b7f983dce3b58b
Nadia Muntaha <nadia031cs@gmail.com>
2026-06-30 11:19:38 +0600

    Update Main5.java

--- Task 5/Main5.java
+++ Task 5/Main5.java
@@ -1,1 +1,54 @@
+interface Researcher{
+    void conductLabResearch();
+}
+
+class UniversityMember{
+        public void attendLecture(){
+            System.out.println("Attending a lecture.");
+        }
+}
+
+class UndergraduateStudent extends UniversityMember{
+    //only attends lecture
+}
+class GraduateResearchAssistant extends UniversityMember implements Researcher{
+    @Override
+    public void conductLabResearch(){
+        System.out.println("Publising papers and running experiments.");
+    }
+}
+class Professor extends UniversityMember implements Researcher{
+    @Override
+    public void conductLabResearch(){
+        System.out.println("Publishing papers and runnnimg experiments");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+public class Main5 {
+   public static void main(String[] args) {
+    
+
+    UniversityMember student = new UndergraduateStudent();
+    student.attendLecture();
+
+    GraduateResearchAssistant gra = new GraduateResearchAssistant();
+    gra.attendLecture();
+    gra.conductLabResearch();
+
+    Professor professor= new Professor();
+    professor.attendLecture();
+    professor.conductLabResearch();
+   } 
+}
 
