c867ab5f7c28feb2d2cac55929b8aa92e03f8fd0
Nadia Muntaha <nadia031cs@gmail.com>
2026-06-30 10:30:20 +0600

    Implement multi-function copier with printing, scanning, and faxing

--- Task 5/Main1.java
+++ Task 5/Main1.java
@@ -1,1 +1,43 @@
+interface Printer{
+    void printDocument();
+}
+interface Scanner{
+    void scanDocument();
+}
+interface FaxMachine{
+    void faxDocument();
+}
+class MultiFunctionCopier implements Printer,Scanner,FaxMachine{
+    
+    @Override
+    public void printDocument(){
+        System.out.println("Printing...");
+    }
+    @Override
+    public void scanDocument(){
+        System.out.println("Scanning...");
+    }
+    @Override
+    public void faxDocument(){
+        System.out.println("Faxing...");
+    }
+    
+    
+}
+class BasicPrinter implements Printer{
+    @Override
+    public void printDocument(){
+        System.out.println("Printing...");
+    }
+}
+public class Main{
+    public static void main(String[] args) {
+        BasicPrinter bp = new BasicPrinter();
+        bp.printDocument();
 
+        MultiFunctionCopier mfc = new MultiFunctionCopier();
+        mfc.printDocument();
+        mfc.scanDocument();
+        mfc.faxDocument();
+    }
+}
